35d7ce0722f9c33ef516303c246b49194936d2cf
msakyol <msakyol@outlook.com>
2021-06-10 20:33:05 +0200

    V1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import UseEffectFetch from "./UseEffectFetch";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseEffect01 /> */}
+      <UseEffectFetch />
+      {/* <UseEffectFetchError /> */}
+      {/* <UseEffectAxios /> */}
+      {/* <UseEffectFetchTxt /> */}
+    </div>
+  );
+}
+export default App;
